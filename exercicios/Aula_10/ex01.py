b34eeb4ecb357ba32a165a2c3ae9d2d52dce7d74
HIKARO CARVALHO <hikarofcarvalho@gmail.com>
2021-06-18 21:36:37 -0300

    correção

--- exercicios/Aula_10/ex01.py
+++ exercicios/Aula_10/ex01.py
@@ -1,10 +1,14 @@
 cadastro = list()
+contador = list([0,0])
 for cont in range(5):
     client = str(input("digite seu nome: "))+","+ str(input("digite sua idade"))
     cadastro.append(client.split(","))
     if int(cadastro[cont][1]) < 18:
         cadastro[cont].append("menor de idade")
+        contador[0] += 1
     else:
         cadastro[cont].append("maior de idade")
+        contador[1] += 1
 for cont in cadastro:
-        print(cont)
+    print(cont)
+print(f"menor de idade tem {contador[0]} \n maior de idade tem {contador[1]}")
