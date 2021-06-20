255129fb3701d7288c160068e71b93708f815dd6
HIKARO CARVALHO <hikarofcarvalho@gmail.com>
2021-06-19 21:15:27 -0300

    updates and study

---
+++ estudos/estudo_03.py
@@ -0,0 +1,32 @@
+# estudando manipulação de listas
+# declaração de listas
+
+
+
+lista = list([["carne",24.95],["arroz",10.00],["feijão",7.35]])
+
+#[["carne",24,95]["arroz",10,00]["feijão",7,35]]
+
+
+#acessando valores em listas
+print(lista[0])
+print(lista[0][2])  
+#                     y
+# _______________     ^+
+# |[2][0]|[2][1]|    2|_______.[2][2]
+# |[1][0]|[1][1]|     |       |
+# |[0][0]|[0][1]| -<__|_._____|__>x +
+#                     |       2
+#                     |-
+#inserção de itens em lista
+
+
+
+
+#removendo valores de listas
+
+
+
+
+#pesquisando em listas
+
