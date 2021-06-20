846f46b20dc2c5c7885ee38de1faa74608a34ca3
HIKARO CARVALHO <hikarofcarvalho@gmail.com>
2021-06-20 13:15:37 -0300

    udate projeto 02

--- projetos/Projeto_02_jogo_Jokenpo.py
+++ projetos/Projeto_02_jogo_Jokenpo.py
@@ -1,4 +1,4 @@
-import random
+from random import randint
 lista = ["pedra","papel","tesoura"]
 iten = ["""                       !@                         
                   @@@@@@@@@                       
@@ -60,29 +60,32 @@ iten = ["""                       !@
              !@      !@:!:@!      @!              
               !#@#!@@@@   @#@#.#@@#               
                                                   """]
-rodar = False
-while rodar:
-    maquina = random.randint(3)
-    jogador = str(input("digite sua opção pedra, papel ou tesoura")).index(lista)
+jogar = True
+while jogar:
+    maquina = randint(0,2)
+    jogador_str = str(input("digite sua opção pedra, papel ou tesoura: \n")).lower()
+    jogador = lista.index(jogador_str)
+    if jogador_str not in lista:
+      print("opção inválida tente novamente")
+      pass
     if maquina == jogador :
-        print(f""""empate.
-                você:{iten[jogador]}
+        print(f""""
+              você:{iten[jogador]}
 
-                maquina: {iten[maquina]}
-                        \n você empatou!""")
-    elif maquina > jogador or maquina == 1 and jogador == 3:
-        print(f""""empate.
+              maquina: {iten[maquina]}
+              \n você empatou!""")
+    elif maquina == 1 and jogador==0 or maquina==2 and jogador==1 or maquina == 0 and jogador == 2:
+        print(f""""
                 você:{iten[jogador]}  
 
-                maquina: {iten[maquina]}
-                        """)
-        print("você perdeu!")
+                maquina:{iten[maquina]}
+        você perdeu!""")
     else:
-        print(f""""empate.
+        print(f""""
                 você:{iten[jogador]}
 
                 maquina: {iten[maquina]}
-                        """)
-        print("você ganhou!")
+        você ganhou!""")
+        
 
-    rodar = input("continuar jogando? sim | não : ").strip().lower().startswith('s')
+    jogar = input("continuar jogando? sim | não : ").strip().lower().startswith('s')
