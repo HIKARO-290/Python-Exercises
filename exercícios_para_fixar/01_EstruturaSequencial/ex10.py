dddee5086ac364caa048bf59f3ddffdc5b948ce7
HIKARO CARVALHO <hikarofcarvalho@gmail.com>
2021-06-21 18:07:02 -0300

    new maked exercicies

---
+++ exercícios_para_fixar/01_EstruturaSequencial/ex10.py
@@ -0,0 +1,9 @@
+#get the value in celsius from user
+#pega o valor em celsius do usuário
+celsius = float(input("digite o valor em graus celsius: "))
+#perform the calculus to convert
+#realiza o calculo para converter
+fahrenheit = (9*celsius/5)+32
+#show the result from the convertion
+#mostra o resultado da conversão
+print("O valor em graus fahrenheit é de: ",fahrenheit)
