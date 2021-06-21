f9d5a62c8fc65703b34c1c43aa6a9befa2331004
HIKARO CARVALHO <hikarofcarvalho@gmail.com>
2021-06-21 19:32:38 -0300

    adicionado comentários ao código

--- projetos/Projeto_02_jogo_Jokenpo.py
+++ projetos/Projeto_02_jogo_Jokenpo.py
@@ -1,5 +1,8 @@
+#importa biblioteca random
 from random import randint
+#gera lista de opções
 lista = ["pedra","papel","tesoura"]
+#gera lista de imagens
 iten = ["""
                   _________                                   
                  /%%%%%%%%/|
@@ -33,14 +36,20 @@ iten = ["""
                     !@! !@!
                     Tesoura           
                                                   """]
+#atribui verdadeiro para loop
 jogar = True
+#inicia loop
 while jogar:
     maquina = randint(0,2)
+#recebe valor de jogada
     jogador_str = str(input("digite sua opção pedra, papel ou tesoura: \n")).lower()
+#verifica se opção existe   
     if jogador_str not in lista:
       print("opção inválida tente novamente")
       continue
+#pega index da escolha do jogador
     jogador = lista.index(jogador_str)
+#verifica quem venceu a jogada e apresenta o que foi jogado
     if maquina == jogador :
         print(f""""
               você:{iten[jogador]}
@@ -60,5 +69,5 @@ while jogar:
                 maquina: {iten[maquina]}
         você ganhou!""")
         
-
+#pergunta se deseja jogar novamente
     jogar = input("continuar jogando? sim | não : ").strip().lower().startswith('s')
