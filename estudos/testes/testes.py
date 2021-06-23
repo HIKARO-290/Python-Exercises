9359ce9be7a125c9f611feeadfa11e7d1f6e9cbf
HIKARO CARVALHO <hikarofcarvalho@gmail.com>
2021-06-23 01:08:57 -0300

    update exercise number 6

--- estudos/testes/testes.py
+++ estudos/testes/testes.py
@@ -1,31 +1,33 @@
-teste = "uma String pAra testes   "
-print(teste)
-print(f"{teste.lower()}")
-print(f"{teste.upper()}")
-print(f"{teste[::-1]}")
-print(f"{teste[1]}")
-print(f"{teste[1:5]}")
-print(f"{teste.title()}")
-print(f"{teste.capitalize()}")
-print(f"{teste.swapcase()}")
-print(f"{teste.ljust(50)}")
-print(f"{teste.split(' ')}")
-print(f"{teste.replace('String','batata')}")
-print(f"{teste.strip()}")
-print(f"{teste.center(50)}")
-print(f"{teste.find('r')}")
-print(f"{teste.startswith('u')}")
-print(f"{teste.endswith('s')}")
-print(f"{teste.islower()}")
-print(f"{teste.isupper()}")
-print(f"{type(teste)}")
-print(f"{teste.count('a')}")
-print(f"{len(teste)}")
+# teste = "uma String pAra testes   "
+# print(teste)
+# print(f"{teste.lower()}")
+# print(f"{teste.upper()}")
+# print(f"{teste[::-1]}")
+# print(f"{teste[1]}")
+# print(f"{teste[1:5]}")
+# print(f"{teste.title()}")
+# print(f"{teste.capitalize()}")
+# print(f"{teste.swapcase()}")
+# print(f"{teste.ljust(50)}")
+# print(f"{teste.split(' ')}")
+# print(f"{teste.replace('String','batata')}")
+# print(f"{teste.strip()}")
+# print(f"{teste.center(50)}")
+# print(f"{teste.find('r')}")
+# print(f"{teste.startswith('u')}")
+# print(f"{teste.endswith('s')}")
+# print(f"{teste.islower()}")
+# print(f"{teste.isupper()}")
+# print(f"{type(teste)}")
+# print(f"{teste.count('a')}")
+# print(f"{len(teste)}")
 
 
 
-listacomp = [["and","gab"],["luc","ant"],["lima","part"]]
-print((listacomp[1][0]))
+# listacomp = [["and","gab"],["luc","ant"],["lima","part"]]
+# print((listacomp[1][0]))
 
-
+teste ="string"
+teste.ljust(50)
+print(f"|{teste.ljust(50)}|")
     
