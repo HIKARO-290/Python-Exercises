dea337f7edd026b1d40723e09ce6f0b35813db97
HIKARO-290 <hikarofcarvalho@gmail.com>
2021-06-25 00:37:00 -0300

    update

--- estudos/testes/testes.py
+++ estudos/testes/testes.py
@@ -1,18 +1,2 @@
-import os
-import locale
-from datetime import date, datetime 
-os.system('cls' if os.name == 'nt' else 'clear')
-locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
-def data(data2):
-    try:
-        data2 = datetime.strptime(data2, '%d/%m/%Y').date()
-        dataFormatada = data2.strftime('%d/%B/%Y')
-    except ValueError:
-        return 'Data Inválida'
-    else:
-        return dataFormatada
-
-
-data2 = input('Digite a data: ')
-#dataFormatada = data(data2)
-print(data(data2))
+from datetime import datetime, date
+print(datetime(2021,2,29))
--- exercicios_Aulas/Aula_14/ex08-Desafio.py
+++ exercicios_Aulas/Aula_14/ex08-Desafio.py
@@ -1,19 +1,31 @@
-import os
-import locale
-from datetime import date, datetime 
-os.system('cls' if os.name == 'nt' else 'clear')
-locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+#DESAFIO - Data com mês por extenso. Construa uma função que receba uma data no
+#formato DD/MM/AAAA e devolva uma string no formato D de mesPorExtenso de AAAA.
+#Opcionalmente, valide a data e retorne NULL caso a data seja inválida. Considere que
+#Fevereiro tem 28 dias e que a cada 4 anos temos ano bisexto, sendo que nesses casos Fevereiro
+#terá 29 dias.
 
-#apply the conversion to date
-def data(valor):    
-# verify if date exist    
-    if datetime.strptime(valor, '%d/%m/%Y'):
-# return the converted date
-        return datetime.strptime(valor, '%d/%m/%Y').date().strftime('%d/%B/%Y')
-    else:
-# if the date doesn't have a valid value return null
+from datetime import datetime
+def convertDate(year,month,day):
+    date = datetime(year,month,day)
+    months=("Janeiro","fevereiro","maço","abril","maio","junho","julho","agosto","setembro","outubro","novembro","dezembro")
+    month = months[date.month-1]
+    return (f"{date.day} de {month} de {date.year}")
+def checkIfTheDateExists(value):
+    date = value.split("/")
+    if int(date[2])%4==0:
+        if int(date[1]) == 2:
+            if date[0]==29:
+                pass
+            else:
+                return "NULL"
+    elif int(date[1]) == 2:
+            if date[0]==28:
+                pass
+            else:
+                return "NULL"
+    if date[1] in [1,3,5,7,8,10,12] and date[0] > 31 or date[1] in [4,6,9,11] and date[0] > 30:
         return "NULL"
+    return convertDate(int(date[2]),int(date[1]),int(date[0]))   
 
-data = input("digite uma data no formato dd/mm/aaaa: ")
-#show the result 
-print(f"A data formatada é: {verificaValidade(data)}")
+dates = input("Enter with one dante using this format: dd/mm/aaaa: ")
+print(f"The date converted is: {checkIfTheDateExists(dates)}")
