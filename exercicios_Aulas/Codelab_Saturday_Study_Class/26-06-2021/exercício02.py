23f215148628b77e3ae75970dd6fa828c97d242e
HIKARO-290 <hikarofcarvalho@gmail.com>
2021-06-26 14:59:54 -0300

    finish the second saturday exercise

--- exercicios_Aulas/Codelab_Saturday_Study_Class/26-06-2021/exercício02.py
+++ exercicios_Aulas/Codelab_Saturday_Study_Class/26-06-2021/exercício02.py
@@ -5,3 +5,44 @@
 # B) A média de idade;
 # C) Uma lista de pessoas com idade acima da média;
 # D) Uma lista com todos os dados cadastrados de todas as pessoas.
+persons = list()
+person = dict(
+        {
+            'name':'',
+            'gender':'',
+            'age':0
+        })
+def register():
+    while True:
+        person['name'] = input("Enter with the name of one person: ")
+        person['gender'] = input("Enter with the gender of this person: ")
+        person['age'] = int(input("Enter with the age of this person: "))
+        persons.append(person)
+        if input("You have another person to register? (Y|N) ").lower().strip(" ").startswith('n'):
+            break
+        
+def createTheResults():
+    results = dict()
+    results['numberofregisters'] = len(persons)
+    results['middle'] = 0
+    results['highages'] = list()
+    for i in persons:
+        results['middle'] +=i['age']/results['numberofregisters']
+    for i in persons:
+        if i['age']>results['middle']:
+            results['highages'].extend(i['name'])
+    return results
+def showTheResults(results = dict()):
+    print("The number of registered persons is:",results['numberofregisters'])
+    print("The middle of ages is:",results['middle'])
+    print("The list of persons with high age is:",results['highages'])
+    for i in persons:
+        print("The name of this person is:", i['name'])
+        print("where the gender is:",i['gender'])
+        print("And the age is:",i['age'],"\n")
+        
+def main():
+    register()
+    showTheResults(createTheResults()) 
+
+main()   
