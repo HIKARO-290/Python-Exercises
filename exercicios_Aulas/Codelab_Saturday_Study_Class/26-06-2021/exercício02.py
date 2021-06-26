94190fe82f2b44fab911d159e3932067c810a05a
HIKARO-290 <hikarofcarvalho@gmail.com>
2021-06-26 08:06:18 -0300

    included archives and descriptions for how are to be the exercise

---
+++ exercicios_Aulas/Codelab_Saturday_Study_Class/26-06-2021/exercício02.py
@@ -0,0 +1,7 @@
+#02 - Crie um programa que leia nome, gênero e idade de várias pessoas,
+# guardando os dados de cada pessoa em um dicionário e todos os dicionários em
+# uma lista. No final, mostre:
+# A) Quantas pessoas foram cadastradas;
+# B) A média de idade;
+# C) Uma lista de pessoas com idade acima da média;
+# D) Uma lista com todos os dados cadastrados de todas as pessoas.
