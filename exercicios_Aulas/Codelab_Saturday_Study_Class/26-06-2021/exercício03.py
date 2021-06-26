d8cc7e2b605007bedfce662fefab9bd90815090e
HIKARO-290 <hikarofcarvalho@gmail.com>
2021-06-26 15:02:30 -0300

    exercise 3 maked

--- exercicios_Aulas/Codelab_Saturday_Study_Class/26-06-2021/exercício03.py
+++ exercicios_Aulas/Codelab_Saturday_Study_Class/26-06-2021/exercício03.py
@@ -3,3 +3,29 @@
 # AAAA. Opcionalmente, valide a data e retorne NULL caso a data seja inválida.
 # Considere que Fevereiro tem 28 dias e que a cada 4 anos temos ano bisexto, sendo
 # que nesses casos Fevereiro terá 29 dias.
+
+from datetime import datetime
+def convertDate(year,month,day):
+    date = datetime(year,month,day)
+    months=("Janeiro","fevereiro","maço","abril","maio","junho","julho","agosto","setembro","outubro","novembro","dezembro")
+    month = months[date.month-1]
+    return (f"{date.day} de {month} de {date.year}")
+def checkIfTheDateExists(value):
+    date = value.split("/")
+    if int(date[2])%4==0 and int(date[2])%400 == 0:
+        if int(date[1]) == 2:
+            if date[0]==29:
+                pass
+            else:
+                return "NULL"
+    elif int(date[1]) == 2:
+            if date[0]==28:
+                pass
+            else:
+                return "NULL"
+    if date[1] in [1,3,5,7,8,10,12] and date[0] > 31 or date[1] in [4,6,9,11] and date[0] > 30:
+        return "NULL"
+    return convertDate(int(date[2]),int(date[1]),int(date[0]))   
+
+dates = input("Enter with one dante using this format: dd/mm/aaaa: ")
+print(f"The date converted is: {checkIfTheDateExists(dates)}")
