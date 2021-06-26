f034b9db5b174f28d9bd799a893748ad5ad38cac
HIKARO-290 <hikarofcarvalho@gmail.com>
2021-06-26 19:56:25 -0300

    saturday exercises done

--- exercicios_Aulas/Codelab_Saturday_Study_Class/26-06-2021/exercício02.py
+++ exercicios_Aulas/Codelab_Saturday_Study_Class/26-06-2021/exercício02.py
@@ -5,6 +5,7 @@
 # B) A média de idade;
 # C) Uma lista de pessoas com idade acima da média;
 # D) Uma lista com todos os dados cadastrados de todas as pessoas.
+#create a list ro person
 persons = list()
 person = dict(
         {
@@ -12,6 +13,7 @@ person = dict(
             'gender':'',
             'age':0
         })
+#create the registers
 def register():
     while True:
         person['name'] = input("Enter with the name of one person: ")
@@ -20,7 +22,7 @@ def register():
         persons.append(person)
         if input("You have another person to register? (Y|N) ").lower().strip(" ").startswith('n'):
             break
-        
+#manipulate the data values from user to create the results        
 def createTheResults():
     results = dict()
     results['numberofregisters'] = len(persons)
@@ -32,6 +34,7 @@ def createTheResults():
         if i['age']>results['middle']:
             results['highages'].extend(i['name'])
     return results
+#Show oll results here
 def showTheResults(results = dict()):
     print("The number of registered persons is:",results['numberofregisters'])
     print("The middle of ages is:",results['middle'])
@@ -40,9 +43,9 @@ def showTheResults(results = dict()):
         print("The name of this person is:", i['name'])
         print("where the gender is:",i['gender'])
         print("And the age is:",i['age'],"\n")
-        
+#function to call sequence of functions       
 def main():
     register()
     showTheResults(createTheResults()) 
-
+#start the process
 main()   
