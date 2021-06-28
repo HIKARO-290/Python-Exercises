a58de7e1857964b864686b961b92cab55a617ca9
HIKARO-290 <hikarofcarvalho@gmail.com>
2021-06-28 11:44:36 -0300

    fixed exercíses 05 and 06 finished

--- estudos/exercícios_para_fixar/02_EstruturaDeDecisao/ex05.py
+++ estudos/exercícios_para_fixar/02_EstruturaDeDecisao/ex05.py
@@ -3,3 +3,14 @@
 # A mensagem "Aprovado", se a média alcançada for maior ou igual a sete;
 # A mensagem "Reprovado", se a média for menor do que sete;
 # A mensagem "Aprovado com Distinção", se a média for igual a dez.
+firstnote = float(input("Enter with the first note: "))
+secondnote = float(input("Enter with the second note: "))
+middle = (firstnote + secondnote) / 2
+if middle == 10:
+    print("Congratulations you have passed with distintion!! Your note is:",middle)
+elif middle >=7:
+    print("You have pass with",middle,"like your note!")
+elif middle <7:
+    print("You have REPROVED with",middle,"like your note!")
+else:
+    print("Some note has an incorrect value!")
