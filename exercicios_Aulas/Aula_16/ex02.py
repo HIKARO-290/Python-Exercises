e97f3670e6b64b6d3461a52242baec98bdb8d2f2
HIKARO-290 <hikarofcarvalho@gmail.com>
2021-06-28 22:13:08 -0300

    update ex02 class 16

--- exercicios_Aulas/Aula_16/ex02.py
+++ exercicios_Aulas/Aula_16/ex02.py
@@ -23,8 +23,25 @@ class Conta:
     def depositarEmConta(self,valor):
         self.saldoconta+=valor
         print("Seu novo saldo é de:",self.saldoconta)
-
-contapessoal01 = Conta("juão",0)
-contapessoal01.depositarEmConta(1300)
-contapessoal01.sacarEmConta(200)
-contapessoal01.sacarEmConta(1200)
+contas = dict()
+while True:
+    if input("você deseja cadastrar uma nova conta?").lower().startswith('n'):
+        break
+    else:
+        titular = input("digite o nome do titular:")
+        contas[titular] = Conta(titular,float(input("digite o Valor inicial desta conta")))
+ 
+    operacao = int(input("que operação deseja realizar?\n01-depositar\n02-sacar "))
+    if operacao == 1:
+        print("digite o nome do títular da conta que deseja realizar a operação")
+        for conta in contas.keys():
+            print(conta)
+        titular = input()
+        contas[titular].depositarEmConta(float(input("digite o valor que deseja depositar: ")))
+    else:
+        print("digite o nome do títular da conta que deseja realizar a operação")
+        for conta in contas.keys():
+            print(conta)
+        titular = input()
+        contas[titular].sacarEmConta(float(input("digite o valor que deseja sacar: ")))
+            
